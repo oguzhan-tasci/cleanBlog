6125f47298f580158961555a71f7d264e3ab3c5c
oyeProjects <oyeprojects@hotmail.com>
2022-07-12 18:09:24 +0300

    I created main file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("Hello WOrld!");
+})
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} icerisinde calismaya basladi...`);
+})
