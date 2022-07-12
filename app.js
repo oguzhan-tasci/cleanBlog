8254263bd52baaa8fb06f9e2e7d8357c6a0cea75
oyeProjects <oyeprojects@hotmail.com>
2022-07-12 22:24:48 +0300

    Static fies uploaded

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Hello WOrld!");
+// Middleware : Projemizde bazı static dosyalar olur bunlar :görseller, html vs. projenin 'makyaj'ı oldugu tarafı olur.
+// Bu verileri dinamik olarak değiştirmek,sıralamak vs. işlemleri yapmak isteriz.
+
+// const myLogger = (req,res,next) => {
+//     console.log("Middleware Log 1");
+//     next(); // eğer next'i yazmazsak bir döngüye giriyor.
+//     // Request - Middleware - Respond arasındakş köprüyü oluştururken sıradaki miiddleware kısmına geçmesi için kullanıyoruz.
+// }
+
+// const myLogger2 = (req,res,next) => {
+//     console.log("Middleware 2");
+//     next();
+// }
+
+// app.use(myLogger);
+// app.use(myLogger2);
+
+// Bu bir 'middleware' kodudur. Sürekli 'request-respond' işlemi yapıyoruz. Bu işlemi döngüye almamıza. özelleştirmemizi sağlayan middleware'dir.
+app.use(express.static("public")); // Bu kodu yazarak 'static' dosyamızı 'public' dosyasına yönlendiriyoruz.
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'temp/index.html'));
 })
 
 
